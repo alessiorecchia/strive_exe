f688b8a8d1beb3ebcdc478bdd52b455f950e2ddb
alessiorecchia <recchia.alessio@yahoo.com>
2021-04-28 08:55:38 +0200

    done some exercises

---
+++ M3_Machine_Learning/07. KNN/non_nbgrader/knn.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+class KNN():
+
+    def __init__(self, k=3):
+        self.k = k
+        self.X_train
+        self.y_train
+    
+    # calculate distance between two vectors
+    def euclidean_distance(self, v1, v2):
+        return np.sqrt(sum((self.v1-self.v2)**2))
+
+    def evaluate(self, y_hat, y_test):
+        return sum(y_hat == y_test) / y_hat.shape[0]
+    
+    def fit():
+        return
+
+    def predict(self, X_test, X_train, y_train):
+        # 
+        y_pred = []
+        for i, v1 in enumerate(X_test):
+            distances = []
+            for j, v2 in enumerate(X_train):
+                distances.append([j ,euclidean_distance(v1, v2)])
+            sorted_dist = sorted(distances, lambda d: d[1])
+            neighbors = sorted_dist[:self.k]
+            labels = y_train[neighbors[:, 0]]
+            y_pred.append(max(set(labels), key=labels.count))
+        return np.array(y_hat)
+
