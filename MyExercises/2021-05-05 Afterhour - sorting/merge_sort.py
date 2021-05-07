556f4eb628e3c42ac84ba52fb1a4cba7ef6c24c5
alessiorecchia <recchia.alessio@yahoo.com>
2021-05-07 18:38:51 +0200

    working on exercises

--- MyExercises/2021-05-05 Afterhour - sorting/merge_sort.py
+++ MyExercises/2021-05-05 Afterhour - sorting/merge_sort.py
@@ -1,40 +1,38 @@
 import numpy as np
 
-def merge(l, p, q, r):
+def merge(A, p, q, r):
     n1 = q - p + 1
     n2 = r - q
-    L = [float('-inf')] * (n1)
-    R = [float('-inf')] * (n2)
-    for i in range(n1):
-        L[i] = l[p + i]
-        for j in range(n2):
-            R[j] = l[q + j]
-            # L[n1] = float('inf')
-            # R[n2] = float('inf')
-            L.append(float('inf'))
-            R.append(float('inf'))
-            i = 0
-            j = 0
-            for k in range(p, r):
-                if L[i] <= R[j]:
-                    l[k] = L[i]
-                    i += 1
-                else:
-                    l[k] = R[j]
-                    j += 1
+    L = [A[n] for n in range(0, n1)] 
+    R = [A[m] for m in range(n2, r + 1)]
+    L.append(float('inf'))
+    R.append(float('inf'))
+    i = 0
+    j = 0
+    for k in range(p, r):
+        if L[i] <= R[j]:
+            A[k] = L[i]
+            i += 1
+        else:
+            A[k] = R[j]
+            j += 1
+
+    return A
 
             
 
 
-def merge_sort(l, p, r):
+def merge_sort(A, p, r):
     if p < r:
         q = (p + r) // 2
-        merge_sort(l, p, q)
-        merge_sort(l, q + 1, r)
-        merge(l, p, q, r)
+        merge_sort(A, p, q)
+        merge_sort(A, q + 1, r)
+        merge(A, p, q, r)
+
+    return A
 
 simple_list = [5, 7, 8, 4, 9, 2, 6, 3, 0, 1]
 
-merge_sort(simple_list, 0, 9)
+merge_sort(simple_list, 0, len(simple_list))
 
 print(simple_list)
