6f25f8e16236917cb4b089dfe64d06db24144d2b
alessiorecchia <recchia.alessio@yahoo.com>
2021-07-01 14:31:10 +0200

    trying to understand web design

--- M8_Full_Stack/03. Flask/main.py
+++ M8_Full_Stack/03. Flask/main.py
@@ -1,65 +1,77 @@
-from flask import Flask,render_template
-
-
+from flask import Flask,render_template,request,redirect,url_for,send_from_directory
+from constants.items import *
+import os
+from werkzeug.utils import secure_filename
+from util.db import read_db,write_project,find_project_by_id,find_project_by_id_and_delete
 app = Flask(__name__)
 
-APP_NAME="alessiorecchia"
-
-social_links = [{
-    "name":"Twitter",
-    "link":"https://twitter.com",
-    "icon":"""<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' fill='currentColor' class='bi bi-twitter' viewBox='0 0 16 16'>
-  <path d='M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z'/>
-</svg>"""
-},{
-    "name":"YouTube",
-    "link":"https://youtube.com",
-    "icon":"""<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' fill='currentColor' class='bi bi-youtube' viewBox='0 0 16 16'>
-  <path d='M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.007 2.007 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.007 2.007 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31.4 31.4 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.007 2.007 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A99.788 99.788 0 0 1 7.858 2h.193zM6.4 5.209v4.818l4.157-2.408L6.4 5.209z'/>
-</svg>"""
-}]
-
-
-
-my_projects = [
-    {
-        "title":"Project A",
-        "description":"Project A description",
-        "cover":"https://analyticsindiamag.com/wp-content/uploads/2021/04/aim_openCV.png",
-        "liveLink":"https://github.com",
-        "githubLink":"https://github.com"
-    },
-     {
-        "title":"Project B",
-        "description":"Project B description",
-        "cover":"http://placehold.it/300x300",
-        "liveLink":"https://github.com",
-        "githubLink":"https://github.com"
-    },
-    {
-        "title":"Project C",
-        "description":"Project C description",
-        "cover":"http://placehold.it/300x300",
-        "liveLink":"https://github.com",
-        "githubLink":"https://github.com"
-    }
-]
-
-# MENU_ITEMS= [{"label":"About","link":"#about"},{"label":"Projects","link":"#projects"},{"label":"Backoffice","link":"/backoffice"}]
+app.config["UPLOAD_FOLDER"]="files"
 
 @app.route("/")
 def index():
-    # return render_template('/views/home.html',APP_NAME=APP_NAME,MENU_ITEMS=MENU_ITEMS,social_links=social_links,my_projects=my_projects)
-    return render_template('/views/home.html',APP_NAME=APP_NAME,social_links=social_links,my_projects=my_projects)
+    return render_template('/views/home.html',APP_NAME=APP_NAME,MENU_ITEMS=MENU_ITEMS,SOCIAL_LINKS=SOCIAL_LINKS,MY_PROJECTS=MY_PROJECTS)
  
-# @app.route("/projects")
-# def projects():
-#     return  render_template("/views/projects/index.html",APP_NAME=APP_NAME,MENU_ITEMS=MENU_ITEMS,my_projects=my_projects)
+@app.route("/dashboard")
+def dashboard():
+    return  render_template("/views/dashboard/index.html",APP_NAME=APP_NAME,DASHBOARD_MENU=DASHBOARD_MENU)
 
 
-# @app.route("/backoffice")
-# def backoffice():
-#     return  "<h1>Backoffice</h1>"
+@app.route("/dashboard/files",methods=["GET","POST"])
+def files():
+    if request.method=="POST":
+        file = request.files["file"]
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config["UPLOAD_FOLDER"],filename))
+        return redirect(url_for("files"))
+       # FİLE UPLOAD
+    else:
+        files = os.listdir(os.path.join(app.config["UPLOAD_FOLDER"]))
+        return render_template("/views/files/index.html",APP_NAME=APP_NAME,DASHBOARD_MENU=DASHBOARD_MENU,files=files)
+
+
+
+@app.route("/dashboard/projects")
+def projects():
+    all_projects = read_db()
+    return render_template("/views/projects/index.html",APP_NAME=APP_NAME,DASHBOARD_MENU=DASHBOARD_MENU,all_projects=all_projects)
+
+
+@app.route("/dashboard/projects/<string:id>",methods=["GET","POST"])
+def project_actions(id):
+    if request.method=="POST":
+        find_project_by_id_and_delete(id)
+        return redirect(url_for("projects"))
+    else:
+        project = find_project_by_id(id)
+        print(project)
+        return redirect(url_for("projects"))
+
+
+@app.route("/dashboard/projects/new",methods=["GET","POST"])
+def new_project():
+    if request.method=="POST":
+        # grab values from form and write into csv file
+        title = request.form.get("title")
+        description = request.form.get("description")
+        cover = request.form.get("cover")
+        githubLink = request.form.get("githubLink")
+        liveLink = request.form.get("liveLink")
+        write_project(title,description,cover,githubLink,liveLink)
+        return redirect(url_for("projects"))
+    else:
+        # display form for adding project
+        return render_template("/views/projects/new.html",APP_NAME=APP_NAME,DASHBOARD_MENU=DASHBOARD_MENU)
+
+
+@app.route("/dashboard/files/<string:filename>",methods=["GET","POST"])
+def file_actions(filename):
+    file_path = os.path.join(app.config["UPLOAD_FOLDER"],filename)
+    if request.method=="POST":
+        os.remove(file_path)
+        return redirect(url_for("files"))
+    else:
+        return send_from_directory(path=app.root_path,directory=app.config["UPLOAD_FOLDER"],filename=filename)
+
 
 
 
