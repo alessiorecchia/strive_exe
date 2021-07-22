647ce8774cdc990d1f6a20979e650640b06e5115
alessiorecchia <recchia.alessio@yahoo.com>
2021-07-22 11:59:19 +0200

    doing exercises

--- M7. NLP/Week 1 - Traditional NLP/3. Chatbot/nltk_utils.py
+++ M7. NLP/Week 1 - Traditional NLP/3. Chatbot/nltk_utils.py
@@ -1,6 +1,9 @@
 import numpy as np
 import nltk
+import spacy
 # nltk.download('punkt')
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 from nltk.stem.porter import PorterStemmer
 stemmer = PorterStemmer()
 
@@ -41,3 +44,40 @@ def bag_of_words(tokenized_sentence, words):
             bag[idx] = 1
 
     return bag
+
+def semantic_sim(u,v):
+    return (u @ v) / (np.sqrt(sum(u**2)) * np.sqrt(sum(v**2)))
+
+def sentence_similarity(sent1, sent2):
+    nlp = spacy.load("en_core_web_sm")
+    
+    # tokenization
+    sent1_list = nlp(sent1) 
+    sent2_list = nlp(sent2)
+    # print('lists: ', sent1_list, sent2_list)
+    
+    # remove stop words from the string
+    sent1_set = {w.lemma_.lower() for w in sent1_list if not w.is_punct and not w.is_stop and not w.text.isdigit()} 
+    sent2_set = {w.lemma_.lower() for w in sent2_list if not w.is_punct and not w.is_stop and not w.text.isdigit()} 
+    # print('sets: ', sent1_set, sent2_set)
+    
+    sent_sims = []
+
+    # form a set containing keywords of both strings  
+    for w1 in sent1_set:
+        word_sim = 0
+        w1 = nlp(w1)
+
+        for w2 in sent2_set:
+            w2 = nlp(w2)
+            # print('words: ', w1, w2)
+            u = w1.vector
+            v = w2.vector
+            ss = semantic_sim(u, v)
+            # print('ss: ', ss)
+            # print(ss > word_sim)
+            if ss > word_sim:
+                word_sim = ss
+        sent_sims.append(word_sim)
+    
+    return(sum(sent_sims)/len(sent_sims))
---
+++ M7. NLP/Week 1 - Traditional NLP/3. Chatbot/test.py
@@ -0,0 +1,8 @@
+import nltk_utils
+
+sent1 = "go left"
+sent2 = "go right"
+
+
+print(nltk_utils.sentence_similarity(sent1, sent2))
+
