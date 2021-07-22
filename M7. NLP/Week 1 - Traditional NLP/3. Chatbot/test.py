5dc4b4e823f08705ee8b994faee8f90d814b2834
alessiorecchia <recchia.alessio@yahoo.com>
2021-07-22 17:50:31 +0200

    doing exercises

--- M7. NLP/Week 1 - Traditional NLP/3. Chatbot/test.py
+++ M7. NLP/Week 1 - Traditional NLP/3. Chatbot/test.py
@@ -1,7 +1,7 @@
 import nltk_utils
 
-sent1 = "go left"
-sent2 = "go right"
+sent1 = "take a walk with the dog"
+sent2 = "I'm coding since 2021, and you?"
 
 
 print(nltk_utils.sentence_similarity(sent1, sent2))
